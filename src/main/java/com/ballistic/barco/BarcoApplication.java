43a4393e879f64740d7af47c04b908f172a99581
NABEEL-AHMED-JMAIL <nabeel.amd@gmail.com>
2018-01-11 18:07:54 +0500

    first commit

---
+++ src/main/java/com/ballistic/barco/BarcoApplication.java
@@ -0,0 +1,12 @@
+package com.ballistic.barco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcoApplication.class, args);
+	}
+}
