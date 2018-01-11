a69cd6fa3c8ead656c1f6490347fe9ccfb6b030c
NABEEL-AHMED-JMAIL <nabeel.amd@gmail.com>
2018-01-12 00:41:35 +0500

    I create image Sprite service but i work tomarrow on this toppic

---
+++ src/main/java/com/ballistic/barco/service/image_sprite/ImageSpriteService.java
@@ -0,0 +1,10 @@
+package com.ballistic.barco.service.image_sprite;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Nabeel on 1/12/2018.
+ */
+@Service
+public class ImageSpriteService {
+}
